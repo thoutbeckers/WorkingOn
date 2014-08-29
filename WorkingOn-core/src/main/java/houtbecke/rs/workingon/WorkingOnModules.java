6586ec2eb1306e2b893bdb9ab8708340050af8b9
Tijl Houtbeckers <tijl@splendo.nl>
2014-08-29 15:26:20 +0200

    fix supporting multiple extra modules

--- WorkingOn-core/src/main/java/houtbecke/rs/workingon/WorkingOnModules.java
+++ WorkingOn-core/src/main/java/houtbecke/rs/workingon/WorkingOnModules.java
@@ -11,5 +11,5 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 @Retention(RUNTIME)
 @Target({ ElementType.TYPE })
 public @interface WorkingOnModules {
-    Class<? extends Module> value();
+    Class<? extends Module>[] value();
 }
